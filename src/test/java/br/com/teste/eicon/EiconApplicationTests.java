39d30ea6d26383bbd0e964eb1cc4439396942685
Furlan <lamfsantos5@gmail.com>
2020-02-28 14:24:30 -0400

    Primeiro commit

---
+++ src/test/java/br/com/teste/eicon/EiconApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.eicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
