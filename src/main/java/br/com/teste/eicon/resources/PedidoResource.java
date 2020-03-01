869e6c5d3a49014c7b21d2f73dc84a497f8a5b3c
Furlan <lamfsantos5@gmail.com>
2020-03-01 00:27:17 -0400

    Consulta com parâmetros opcionais funcionando, porém tá retornando uma soma das listas de cada parâmetro e não um filtro. Regras de negócio movidas para o pacote de serviço.

--- src/main/java/br/com/teste/eicon/resources/PedidoResource.java
+++ src/main/java/br/com/teste/eicon/resources/PedidoResource.java
@@ -1,10 +1,6 @@
 package br.com.teste.eicon.resources;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -12,10 +8,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,63 +24,35 @@ public class PedidoResource {
 	@Autowired
 	private PedidoService service;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE,
-			MediaType.APPLICATION_XML_VALUE })
-	@ResponseBody
-	public ResponseEntity<Pedido> findXml(@PathVariable Integer id) {
-		Pedido pedido = service.findExternal(id);
-		return ResponseEntity.ok().body(pedido);
-	}
+//	@RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE,
+//			MediaType.APPLICATION_XML_VALUE })
+//	@ResponseBody
+//	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
+//		Pedido pedido = service.findExternal(id);
+//		return ResponseEntity.ok().body(pedido);
+//	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<List<String>> insert(@Valid @RequestBody List<Pedido> pedidos) {
-		List<String> response = new ArrayList<String>();
-
-		if (pedidos.size() >= 1 && pedidos.size() <= 10) {
-			for (Pedido pedido : pedidos) {
-				if (service.find(pedido.getNumeroControle()) == null) {
-
-					if (pedido.getQuantidade() == null) {
-						pedido.setQuantidade(1);
-					}
-
-					if (pedido.getDataCadastro() == null) {
-						Date date = Calendar.getInstance().getTime();
-						DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-						pedido.setDataCadastro(dateFormat.format(date));
-					}
-
-					pedido.setValorTotalDoPedido(pedido.getValor() * pedido.getQuantidade());
-
-					if (pedido.getQuantidade() >= 10) {
-						pedido.setValorTotalDoPedido(
-								pedido.getValorTotalDoPedido() - (pedido.getValorTotalDoPedido() * 10 / 100));
-					} else if (pedido.getQuantidade() > 5) {
-						pedido.setValorTotalDoPedido(
-								pedido.getValorTotalDoPedido() - (pedido.getValorTotalDoPedido() * 5 / 100));
-					}
-
-					service.insert(pedido);
-					response.add("Pedido cadastrado: " + pedido.toString());
-				} else {
-					response.add("Pedido não cadastrado, Numero de Controle já cadastrado: " + pedido.toString());
-				}
-			}
-		} else if (pedidos.size() < 1) {
-			response.add("Nenhum pedido cadastrado, o tamanho minimo da lista de pedidos é 1.");
-		} else {
-			response.add("Nenhum pedido cadastrado, o tamanho máximo da lista de pedidos é 10.");
-		}
-
-		return ResponseEntity.ok().body(response);
+		return service.insertList(pedidos);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE })
 	@ResponseBody
-	public ResponseEntity<List<Pedido>> findAll() {
-		List<Pedido> list = service.findAll();
+	public ResponseEntity<List<Pedido>> find(
+			@RequestParam(value = "numeroControle", required = false) Integer numeroControle,
+			@RequestParam(value = "dataCadastro", required = false) String dataCadastro,
+			@RequestParam(value = "cliente", required = false) Integer cliente) {
+		List<Pedido> list = new ArrayList<Pedido>();
+		
+		if (numeroControle == null && dataCadastro == null && cliente == null) {
+			list = service.findAll();
+		} else {
+			list = service.findWithFiter(numeroControle, dataCadastro, cliente);
+		}
+
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/br/com/teste/eicon/services/PedidoService.java
+++ src/main/java/br/com/teste/eicon/services/PedidoService.java
@@ -1,9 +1,15 @@
 package br.com.teste.eicon.services;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.teste.eicon.domain.Pedido;
@@ -19,14 +25,112 @@ public class PedidoService {
 		return pedido.orElse(null);
 	}
 	
+	public Boolean isNotInTheList(List<Pedido> pedidos, Pedido pedido) {
+		for (Pedido p : pedidos) {
+			if(p.equals(pedido)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public List<Pedido> findWithFiter(Integer numeroControle, String dataCadastro, Integer cliente) {
+		List<Pedido> pedidos = findAll();
+		List<Pedido> response = new ArrayList<Pedido>();
+
+		for (Pedido p : pedidos) {
+
+			if (numeroControle != null) {
+				if (numeroControle == p.getNumeroControle()) {
+					if(isNotInTheList(response, p)) {
+						response.add(p);
+					}
+				}
+			}
+
+			if (dataCadastro != null) {
+				String data = "";
+				data = String.valueOf(dataCadastro.charAt(0)) + dataCadastro.charAt(1) + "/" + dataCadastro.charAt(2)
+						+ dataCadastro.charAt(3) + "/" + dataCadastro.charAt(4) + dataCadastro.charAt(5)
+						+ dataCadastro.charAt(6) + dataCadastro.charAt(7);
+				if (data.equals(p.getDataCadastro())) {
+					if(isNotInTheList(response, p)) {
+						response.add(p);
+					}
+				}
+			}
+
+			if (cliente != null) {
+				if (cliente == p.getCodigoCliente()) {
+					if(isNotInTheList(response, p)) {
+						response.add(p);
+					}
+				}
+			}
+
+		}
+
+		return response;
+	}
+
+	public Pedido findByData() {
+		return null;
+	}
+
+	public Pedido findByCliente() {
+		return null;
+	}
+
 	public Pedido findExternal(Integer id) {
 		Optional<Pedido> pedido = repo.findById(id);
-		return pedido.orElseThrow(() -> new br.com.teste.eicon.services.exceptions.
-				ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + Pedido.class.getName()));
+		return pedido.orElseThrow(() -> new br.com.teste.eicon.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + " Tipo: " + Pedido.class.getName()));
 	}
 
 	public Pedido insert(Pedido obj) {
 		return repo.save(obj);
+	}
+
+	public ResponseEntity<List<String>> insertList(List<Pedido> pedidos) {
+		List<String> response = new ArrayList<String>();
+
+		if (pedidos.size() >= 1 && pedidos.size() <= 10) {
+			for (Pedido pedido : pedidos) {
+				if (find(pedido.getNumeroControle()) == null) {
+
+					if (pedido.getQuantidade() == null) {
+						pedido.setQuantidade(1);
+					}
+
+					if (pedido.getDataCadastro() == null) {
+						Date date = Calendar.getInstance().getTime();
+						DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+						pedido.setDataCadastro(dateFormat.format(date));
+					}
+
+					pedido.setValorTotalDoPedido(pedido.getValor() * pedido.getQuantidade());
+
+					if (pedido.getQuantidade() >= 10) {
+						pedido.setValorTotalDoPedido(
+								pedido.getValorTotalDoPedido() - (pedido.getValorTotalDoPedido() * 10 / 100));
+					} else if (pedido.getQuantidade() > 5) {
+						pedido.setValorTotalDoPedido(
+								pedido.getValorTotalDoPedido() - (pedido.getValorTotalDoPedido() * 5 / 100));
+					}
+
+					insert(pedido);
+					response.add("Pedido cadastrado: " + pedido.toString());
+				} else {
+					response.add("Pedido não cadastrado, Numero de Controle já cadastrado: " + pedido.toString());
+				}
+			}
+		} else if (pedidos.size() < 1) {
+			response.add("Nenhum pedido cadastrado, o tamanho minimo da lista de pedidos é 1.");
+		} else {
+			response.add("Nenhum pedido cadastrado, o tamanho máximo da lista de pedidos é 10.");
+		}
+
+		return ResponseEntity.ok().body(response);
 	}
 
 	public List<Pedido> findAll() {
